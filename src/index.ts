45f615448457fa247ff2310c9feeb801d5d3039e
Robert Soriano <sorianorobertc@gmail.com>
2022-07-06 11:38:15 -0700

    feat: remove svelte/store dep

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,15 @@
 import type { EnhancedStore } from '@reduxjs/toolkit'
-import type { Readable } from 'svelte/store'
-import { readable } from 'svelte/store'
 
 const toSvelteStore = <T extends EnhancedStore<any, any, any>>(
   store: T,
-): {
-  subscribe: Readable<ReturnType<T['getState']>>['subscribe']
-  dispatch: T['dispatch']
-} => {
-  const state = readable(store.getState(), (set) => {
-    return store.subscribe(() => {
-      set(store.getState())
-    })
-  })
-
+) => {
   return {
-    subscribe: state.subscribe,
+    subscribe(fn: (payload: ReturnType<T['getState']>) => void) {
+      fn(store.getState())
+      return store.subscribe(() => {
+        fn(store.getState())
+      })
+    },
     dispatch: store.dispatch,
   }
 }
